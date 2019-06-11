4ca53222f01dd66628929a7d40a30cb6d58e3a74
clezyanna <cleziana123@gmail.com>
2019-06-11 17:55:38 -0300

    Implementando uma Interface

---
+++ from-java-to-dart/Questao5/Questao5.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+
+abstract class Area {
+ factory Area(String type) {
+    if (type == 'circulo') return Circulo(2);
+    if (type == 'quadrado') return Quadrado(2);
+    throw 'Can\'t create $type.';
+  }
+  num get area;
+}
+
+class Circulo implements Area {
+  final num radio;
+  Circulo(this.radio);
+  num get area => pi * pow(radio, 2);
+}
+
+class Quadrado implements Area {
+  final num side;
+  Quadrado(this.side);
+  num get area => pow(side, 2);
+}
+class CirculoMock implements Circulo{
+  num area;
+  num radio;
+}
+
+
+main() {
+  final circulo = Area('circulo');
+  final quadrado = Area('Quadrado');
+  print(circulo.area);
+  print(quadrado.area);
+}
